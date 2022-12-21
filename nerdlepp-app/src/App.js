b0ead881a4b496388e0850a734d25cc33fe86209
siddhakarkare <siddhakarkare@gmail.com>
2022-12-21 12:03:02 -0500

    UI Beautification
    - CSS classes updated
    - ButtonPress event added
    - state variables created for grid

--- nerdlepp-app/src/App.js
+++ nerdlepp-app/src/App.js
@@ -1,37 +1,44 @@
 // import logo from './icon.svg';
 import './App.css';
-import {Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 import Settings from './Settings'
 import GameGrid from './GameGrid';
-import {  Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import React, { Component } from 'react';
 
-function App() {
-  
-  return (
-    <div className="App">
-      <Router>
-        <Navbar bg="primary" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home">Nerdle ++</Navbar.Brand>
-            
-            <Nav className="me-auto">
-              <Nav.Link to="/settings">Settings</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-        
+class App extends Component {
 
-        <GameGrid>
+  constructor() {
+    super()
+  }
+  render = () => {
+    return (
+      <div className="App">
+        <Router>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand href="/">Nerdle ++</Navbar.Brand>
 
-        </GameGrid>
+              <Nav className="me-auto">
+                <Nav.Link to="/settings">Settings</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
 
 
-        <Routes>
-          <Route path="/settings" component={Settings} ></Route> 
-        </Routes>
-      </Router>
-    </div>
-  );
+          <GameGrid>
+
+          </GameGrid>
+
+
+          <Routes>
+            <Route path="/settings" component={Settings} ></Route>
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- nerdlepp-app/src/GameGrid.js
+++ nerdlepp-app/src/GameGrid.js
@@ -4,46 +4,82 @@ import gameConfig from './gameConfig.json';
 
 
 class GameGrid extends Component {
+    n = 0
+    curr = "0,0"
+    currRow = 0
+    grid = [[]]
     constructor() {
         super();
+        this.n = "5"
+        this.curr = "0,0"
+        this.currRow = 0
         this.state = {
             n: "5",
             curr: "0,0",
-            rows: []
-
+            grid : [[]]
         };
+        this.grid = [[]]
 
-        console.log(gameConfig[this.state.n])
+        // console.log("GameConfig:",gameConfig[this.state.n])
     }
 
     gameChanged = (e) => {
         // e.preventDefault();
         console.log("Game Changed", e);
-        this.setState({
-            n: e,
-            rows: e
-        })
+        
+        this.state.n = e
+        this.n = e
+        
         this.render();
 
     };
     setCurr = (e) => {
-        console.log("FocusChanged", e)
+        console.log("Setting Current", e)
         this.state.curr = e
+        this.curr = e
     }
-
-    createTable = () => {
-        console.log("N:", this.state.n)
-        let table = []
-
-        for (let i = 0; i < this.state.n; i++) {
-            let children = []
-            for (let j = 0; j < this.state.n; j++) {
-                children.push(<td><input size="sm" id={i + "," + j} /></td>)
+    inputSelected = (e) => {
+        console.log("Input Selected:",e.target.id)
+        this.setCurr(e.target.id)
+    }
+    createGrid = () =>{
+        let row = []
+        let grid = []
+        for (let i = 0; i < this.n; i++) {
+            row = []
+            for (let j = 0; j < this.n; j++) {
+                row.push("")
             }
-            table.push(<tr>{children}</tr>)
+            grid.push(row)
+        }
+        this.state.grid = grid
+        this.grid = grid
+
+    }
+    createTable = () => {
+        this.createGrid()
+
+        let table = []
+        
+        this.currRow = 0
+        for (let i = 0; i < this.n; i++) {
+            let children = []
+            for (let j = 0; j < this.n; j++) {
+                let flag = true
+                if(i===0){
+                    flag = false
+                    console.log("First row")
+                }
+                children.push(<td key={"td"+i + "," + j}><input value={this.state.grid[i][j]} onFocus={this.inputSelected} 
+                onChange={this.valueChanged} className= "inputClass" maxLength="1" size="sm" id={i + "," + j} /></td>)
+            }
+            table.push(<tr key={"tr"+i}>{children}</tr>)
         }
         return table
 
+    }
+    valueChanged = (e) => {
+        console.log("Dummy event", e.target)
     }
     createNumbers = () => {
         console.log("Creating Numbers")
@@ -51,42 +87,57 @@ class GameGrid extends Component {
         let numbers = []
 
         for (let i = 0; i <= 9; i++) {
-            numbers.push(<button onClick={this.buttonPressed}>{i}</button>)
+            numbers.push(<button id={"button"+i} key={"button"+i} onClick={this.buttonPressed}>{i}</button>)
         }
         return numbers
 
     }
     buttonPressed = (e) => {
-        console.log("Button Pressed", e)
+        let txt = e.target.innerHTML
+        this.state.userInput = txt
+
+        let loc = this.curr.split(",")
+        let i = loc[0]
+        let j = loc[1]
+
+        
+
+        this.state.grid[i][j] = txt
+        console.log("Button Pressed", e, txt)
+        console.log("Setting grid at (",i,",",j,") with value=", this.state.grid[i][j])
     }
 
     createOperators = () => {
         let operators = []
-        let op = gameConfig[this.state.n]["operators"]
+        let op = gameConfig[this.n]["operators"]
 
         for (let i in op) {
-            operators.push(<button onClick={this.buttonPressed}>{op[i]}</button>)
+            operators.push(<button className="operatorButton" id={"button"+op[i]} key={"button"+op[i]} onClick={this.buttonPressed}>{op[i]}</button>)
         }
 
         return operators
     }
     createEnterDelete = () => {
         let enterDelete = []
-        enterDelete.push(<button onClick={this.enterPressed}>Enter</button>)
-        enterDelete.push(<button onClick={this.deletePressed}>Delete</button>)
+        enterDelete.push(<button className="enterDeleteButton" id="buttonEnter" key="buttonEnter" onClick={this.enterPressed}>Enter</button>)
+        enterDelete.push(<button className="enterDeleteButton" id="buttonDelete" key="buttonDelete" onClick={this.deletePressed}>Delete</button>)
         return enterDelete
     }
 
     enterPressed = (e) => {
         // let c = this.curr
-        console.log(e)
+        console.log("Enter Pressed",e)
+    }
+    resetGrid = () => {
+        this.curr ="0,0"
+        this.currRow = 0
     }
     render() {
-
+        this.resetGrid()
         return (
             <div>
-                <div style={{ float: 'right', paddingTop: 0.5 + 'rem' }}>
-                    <DropdownButton title="Change Game" onSelect={this.gameChanged}>
+                <div className='changeGameDropDown'>
+                    <DropdownButton key="gameChangeButton" title="Change Game" onSelect={this.gameChanged} variant="outline-dark" size="sm">
                         <Dropdown.Header>Game Type</Dropdown.Header>
                         <Dropdown.Item eventKey="5">5X5</Dropdown.Item>
                         <Dropdown.Item eventKey="6">6X6</Dropdown.Item>
@@ -99,12 +150,13 @@ class GameGrid extends Component {
                     <div className='numberContainer'>
                         {this.createNumbers()}
                     </div>
-                    <center className='operatorContainer'>
-                        {this.createOperators()}
-                    </center>
-                    <center className="operatorContainer">
-                        {this.createEnterDelete()}
-                    </center>
+                    <div className="container">
+                        <center className='operatorContainer col '>
+                            {this.createEnterDelete()}
+                            {this.createOperators()}
+                            
+                        </center>
+                    </div>
                     <center>
                         <table>
                             <tbody>
